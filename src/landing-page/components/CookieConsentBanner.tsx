77e317fb89b81d78e9f60255182f7192efb5659f
3umaGH <3uma+github@protonmail.com>
2024-10-16 16:56:20 +0300

    refactor: clean up

--- src/landing-page/components/CookieConsentBanner.tsx
+++ src/landing-page/components/CookieConsentBanner.tsx
@@ -1,57 +1,16 @@
-import { useState, useRef } from 'react';
-import { cn } from '../cn';
-
 const MovingCookieConsentBanner = () => {
-  const [position, setPosition] = useState({ top: 0, left: 0 });
-  const [hasMousedOver, setHasMousedOver] = useState(false);
-  const divRef = useRef<HTMLDivElement>(null);
-
-  const handleMouseOver = () => {
-    if (!hasMousedOver) setHasMousedOver(true)
-    const divWidth = divRef.current ? divRef.current.offsetWidth : 0;
-    const divHeight = divRef.current ? divRef.current.offsetHeight : 0;
-
-    const newPosition = {
-      top: Math.random() * (window.innerHeight - divHeight - 30), // subtract divHeight
-      left: Math.random() * (window.innerWidth - divWidth - 30), // subtract divWidth
-    };
-    console.log('new position: ', newPosition);
-    setPosition(newPosition);
-  };
-
   return (
-    <div
-      style={{ position: 'absolute', top: position.top, left: position.left }}
-      onMouseOver={handleMouseOver}
-    >
-      <div
-        className={cn(
-          {
-            'fixed bottom-0 left-0 w-full bg-blue-500 text-white p-4 flex justify-between items-center ':
-              !hasMousedOver,
-          },
-          {
-            'min-w-full bg-blue-500 text-white p-4 flex justify-between items-center ':
-              hasMousedOver,
-          }
-        )}
-      >
+    <div style={{ position: 'absolute' }}>
+      <div className={'fixed bottom-0 left-0 w-full bg-blue-500 text-white p-4 flex justify-between items-center '}>
         <div>
-          <p className="font-bold">This website uses cookies</p>
-          <p>
-            We use cookies to ensure you get the best experience on our website.
-          </p>
+          <p className='font-bold'>This website uses cookies</p>
+          <p>We use cookies to ensure you get the best experience on our website.</p>
         </div>
-        <button
-          disabled={hasMousedOver}
-          className="bg-white text-blue-500 px-4 py-2 rounded"
-        >
-          Accept
-        </button>
+        <button className='px-4 py-2 text-blue-500 bg-white rounded'>Accept</button>
       </div>
       {/* <div id='cookieconsent'></div> */}
     </div>
-  );
-};
+  )
+}
 
-export default MovingCookieConsentBanner;
+export default MovingCookieConsentBanner
